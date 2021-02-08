a7d504886c2888c0545879a7a4581ad874499e99
Millán Santos Arias <millan.santos@gmail.com>
2021-02-08 17:14:02 +0100

    Update workflow

--- src/test/java/Selenium/Demo/SeleniumDemo.java
+++ src/test/java/Selenium/Demo/SeleniumDemo.java
@@ -1,53 +1,77 @@
 package Selenium.Demo;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SeleniumDemo
 {
-    private WebDriver driver;
+	private WebDriver driver;
 
-    @BeforeClass
-    public void setUp()
-    {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
+	@BeforeClass
+	public void setUp()
+	{
+		WebDriverManager.chromedriver().setup();
+		//To run in Github Action uncomment these liness
+		ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-        driver.navigate().to("https://the-internet.herokuapp.com/login");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(120, TimeUnit.MILLISECONDS);
-    }
+		//driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(120, TimeUnit.MILLISECONDS);
+		driver.get("https://nosoftware-data-7156-dev-ed.cs102.my.salesforce.com/");
+	}
 
-    @Test
-    public void userLogin()
-    {
-        WebElement usernameTxt = driver.findElement(By.id("username"));
-        usernameTxt.sendKeys("tomsmith");
-        WebElement passwordTxt = driver.findElement(By.id("password"));
-        passwordTxt.sendKeys("SuperSecretPassword!");
-        WebElement submitBtn = driver.findElement(By.className("radius"));
-        submitBtn.click();
-        System.out.println("Current URL is:" + driver.getCurrentUrl());
-        Assert.assertTrue(driver.getCurrentUrl().contains("secure"));
-    }
+	@Test
+	public void userLogin() throws InterruptedException
+	{
+		WebElement usernameTxt = driver.findElement(By.id("username"));
+		usernameTxt.sendKeys("test-4xqgtapsgk0j@example.com");
+		WebElement passwordTxt = driver.findElement(By.id("password"));
+		passwordTxt.sendKeys("iY^y)a7I7");
+		driver.findElement(By.id("Login")).click();
 
-    @AfterClass
-    public void tearDown(){
-        if (driver != null) {
-            driver.quit();
-        }
-    }
+		
+		if(driver.findElements(By.partialLinkText ("I Don't Want to Register My Phone")).size() != 0) {
+			driver.findElement(By.partialLinkText ("I Don't Want to Register My Phone")).click();
+		}
+		
+		if(driver.findElements(By.className("continue") ).size() != 0) {
+			driver.findElement(By.className("continue")).click();
+		}
+		
+		//navigateViaAppLauncher(By.xpath("//a[@data-label='Accounts']"));
+		
+		Thread.sleep(2000);
+	}
+
+	@AfterClass
+	public void tearDown(){
+		if (driver != null) {
+			driver.quit();
+		}
+	}
+
+	public void jsClick(By element) {
+		JavascriptExecutor executor = (JavascriptExecutor)driver;
+		executor.executeScript("arguments[0].click();", element);
+	}
+
+	public void navigateViaAppLauncher(By element) {
+		driver.findElement(By.xpath("//nav[contains(@class,'appLauncher')]//button")).click();
+		jsClick(By.xpath("//button[text()='View All']"));
+		jsClick(element);
+	}
 }
